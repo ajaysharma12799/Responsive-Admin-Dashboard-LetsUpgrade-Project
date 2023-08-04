7a6800264818ac4a1daef3d1f3693f805c5cedec
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-08-04 12:36:53 +0530

    added Theme Toggler Working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const sideMenu = document.querySelector("aside");
 const menuButton = document.querySelector("#menu-btn");
 const closeButton = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
 menuButton.addEventListener("click", () => {
   sideMenu.style.display = "block";
@@ -8,4 +9,11 @@ menuButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   sideMenu.style.display = "none";
+});
+
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variable");
+
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
